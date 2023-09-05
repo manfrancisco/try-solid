7df58cd3e766ee1067ddfb6d786430c2027670b3
Michael Dorst <m@mdorst.net>
2023-09-05 01:45:00 -0700

    Create Card

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+export default function Card() {
+  return (
+    <div>
+      <h2>Card Component</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum eligendi,
+        debitis odio pariatur dolore alias!
+      </p>
+    </div>
+  );
+}
