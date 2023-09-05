8cf6581c0a3dafa3cd5d7c2fea434dfd35769cdb
Michael Dorst <m@mdorst.net>
2023-09-05 11:59:49 -0700

    Dynamically load products

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,38 @@
-import { createResource } from "solid-js";
+import { For, Show, createResource } from "solid-js";
 import Card from "../components/Card";
 
 export default function Home() {
   const [products] = createResource(() =>
-    fetch("https://fakestoreapi.com/products").then((res) => res.json()),
+    fetch("http://fakestoreapi.com/products").then((res) => res.json()),
   );
 
   return (
-    <div class="grid grid-cols-4 gap-10 my-4">
-      <Card rounded={true} flat={false}>
-        <h2>Ninja Tee, Black</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam
-          delectus eius laborum officiis cupiditate neque.
-        </p>
-        <button class="btn">Click Me!</button>
-      </Card>
-      <Card rounded={false} flat={true}>
-        <h2>Ninja Tee, White</h2>
-        <button class="btn">Click Me!</button>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam
-          delectus eius laborum officiis cupiditate neque.
-        </p>
-        <p>Only $10</p>
-      </Card>
-
-      <p>{console.log(products(), products.loading)}</p>
-    </div>
+    <Show
+      when={products()}
+      fallback={
+        <div class="my-4">
+          <Card rounded={true}>
+            <h2>Loading...</h2>
+          </Card>
+        </div>
+      }
+    >
+      <div className="grid grid-cols-4 gap-10 my-4">
+        <For each={products()}>
+          {(product) => (
+            <Card rounded={true} flat={true}>
+              <div class="flex flex-col h-full">
+                <img
+                  class="p-3 grow self-center"
+                  src={product.image}
+                  alt={product.title}
+                />
+                <h2 class="my-3">{product.title}</h2>
+              </div>
+            </Card>
+          )}
+        </For>
+      </div>
+    </Show>
   );
 }
