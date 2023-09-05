e90a96ff44b616b1cc69144f0b62f163ebb1849a
Michael Dorst <m@mdorst.net>
2023-09-05 02:16:06 -0700

    Add styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,12 @@
 export default function Card() {
   return (
-    <div>
+    <div class="bg-white p-4 text-center rounded-md shadow-md">
       <h2>Card Component</h2>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum eligendi,
         debitis odio pariatur dolore alias!
       </p>
+      <button class="btn">Click Me!</button>
     </div>
   );
 }
